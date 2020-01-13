d7fee636551eed5becd2a3c89fa5fce0864f9bca
mindatasleep <mindatasleep@gmail.com>
2020-01-13 12:59:03 -0700

    Fix spacing.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Smooth Peaks`,
   },
   plugins: [
     {
